d41de9157ce602d3e704b53af39948a96ad65fe6
Arnav3342 <156603151+Arnav3342@users.noreply.github.com>
2026-01-23 01:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,17 @@ function submitQuiz() {
   // Example score (yaha apna real score logic lagega)
   var score = 72;
 
-  fetch("https://script.google.com/macros/s/AKfycbwJY5p3ckZ9L2sY2--mgx1YKz_Pfx2qXsQ6c7W1u6Re6Jos_fhNCHlKpffLGn2iMWmdfA/exec", {
-    method: "POST",
-    body: JSON.stringify({
-      name: name,
-      score: score
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
+  fetch("https://script.google.com/macros/s/AKfycbyf86GWAub8y-sPXjY6HZxMEgv0Bg7BHAf4vQpGM9oggfz5MtXQSNZD9f5vSYggBzhy/exec", {
+  method: "POST",
+  body: JSON.stringify({
+    name: name,
+    score: score
+  }),
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+
   .then(res => res.json())
   .then(data => {
     document.getElementById("result").innerHTML =
